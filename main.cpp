f4fca5772a95cb03e031f3310625fee5c49519ac
Rohil Patel <patelrohil2006@icloud.com>
2021-02-06 22:10:37 +0000

    Finished lexer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 #include "src/lexer.hpp"
 
 int main() {
-  Lexer lexer("bruh49");
-
-  //lexer.f();
+  Lexer lexer("i^2");
+  
+  std::vector<Token> tokens = lexer.tokenize();
+  
+  for (int i = 0; i < tokens.size(); i++) {
+  	std::cout << "Type: " << tokens[i].type << ", Value: " <<  tokens[i].value << std::endl;
+  }
   return 0;
 }
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -6,13 +6,25 @@
 
 std::string integers = "1234567890";
 std::string specialCharacters = "ei";
-std::string operations = "+-*/%^()";
+std::string operations = "+-*/%^()!";
+
+class Token {
+	public:
+	std::string type;
+	std::string value;
+	
+	Token(std::string typeName, std::string val) {
+		type = typeName;
+		value = val;
+	}
+};
 
 class Lexer {
   private:
   std::string context;
   int index = 0;
   char character;
+  std::vector <Token> tokens;
     
   public:
     Lexer(std::string input) {
@@ -22,8 +34,9 @@ class Lexer {
 
     char advance(int amount = 1) {
       index += amount;
+      character = context[index];
 
-      return context[index];
+      return character;
     }
 
     char peek(int amount = 1) {
@@ -60,10 +73,64 @@ class Lexer {
       return true;
     }
 
-    std::vector <std::unordered_map<std::string, std::string> > tokenize() {
-
-      if (isInteger(character)) {
+    std::vector <Token> tokenize() {
+    	while (character != '\0') {
+    		int lastIndex = index;
+    		
+        if (isInteger(character)) {
+          std::string num(1, character);
+          
+          advance();
         
+         while(character != '\0' && isInteger(character)) {
+           num += character;
+         	
+           advance();
+         }
+         
+         if (character == '.') {
+           num += character;
+           advance();
+         	
+         	 while (character != '\0' && isInteger(character)) {
+         	   num += character;
+         	
+         	   advance();
+           }
+         }
+         
+         tokens.push_back(Token("Number", num));
+        }
+        
+        if (isWhitespace(character)) advance();
+        
+        if (isOperator(character)) {
+        	std::string op(1, character);
+        	
+        	advance();
+        	
+        	if (isOperator(character)) {
+        		op += character;
+        		advance();
+        	}
+        	
+        	tokens.push_back(Token("Operator", op));
+        }
+        
+        if (isSpecialCharacter(character)) {
+        	std::string specChar(1, character);
+        	tokens.push_back(Token("SpecialCharacter", specChar));
+        	
+        	advance();
+        }
+        
+        if (lastIndex == index) {
+          std::cout << "[ERROR] Character " << character << "is undefined.";
+        }
       }
+      
+      tokens.push_back(Token("End of Statement", "End of Statement"));
+      
+      return tokens;
     }
 };
